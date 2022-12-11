6fa92cea428a5c08bef454b9fb86802081cbe66b
Fabrice Payet <hello@fabricepayet.fr>
2022-12-11 22:44:39 +0400

    Fix crisp integration

--- client/components/layouts/Crisp.tsx
+++ client/components/layouts/Crisp.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Script from 'next/script';
-import crispScript from '../../scripts/crisp';
 
 const Crisp = () => {
   return (
-    <Script src={crispScript} />
+    <Script src={'/scripts/crisp.js'} />
   );
 };
 
