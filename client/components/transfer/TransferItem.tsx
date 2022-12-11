4a31d60bb77debf63c2196378c487e6b819086d4
Fabrice Payet <hello@fabricepayet.fr>
2022-12-11 10:12:08 +0400

    Update TransferItem icon

--- client/components/transfer/TransferItem.tsx
+++ client/components/transfer/TransferItem.tsx
@@ -3,7 +3,8 @@ import ITransfer from '../../interfaces/transfer.interface';
 import dayjs from 'dayjs'
 import { ModalEnum } from '../../enums/modal.enum';
 import { LayoutContext } from '../../contexts/layout.context';
-import { FaChevronRight } from 'react-icons/fa';
+import { FaCheckCircle, FaChevronRight, FaQuestionCircle, FaTimesCircle } from 'react-icons/fa';
+import { TransferStatus } from '../../enums/transfer-status.enum';
 
 const TransferItem: FC<{ transfer: ITransfer }> = ({ transfer }) => {
   const { openModal } = useContext(LayoutContext);
@@ -14,8 +15,26 @@ const TransferItem: FC<{ transfer: ITransfer }> = ({ transfer }) => {
     <div className="flex flex-col gap-2 px-4 py-3 border border-gray-200 p-2 cursor-pointer rounded-2xl" onClick={openDetails}>
       <div className="flex justify-between items-center " onClick={openDetails}>
         <div className="flex gap-2" >
-          <div className="rounded-full h-12 w-12 text-2xl font-bold text-white bg-blue-main flex items-center justify-center">
-            {`${transfer.recipient.firstName[0]}${transfer.recipient.lastName[0]}`}
+
+          <div className="flex justify-center items-center rounded-full h-12 w-12">
+            {
+              transfer.status === TransferStatus.FAILED || transfer.status === TransferStatus.OFFRAMP_COMPLETED ? (
+                <>
+                  {
+                    transfer.status === TransferStatus.FAILED && (
+                      <FaTimesCircle className="text-4xl text-red-400" />
+                    )
+                  }
+                  {
+                    transfer.status === TransferStatus.OFFRAMP_COMPLETED && (
+                      <FaCheckCircle className="text-4xl text-green-main" />
+                    )
+                  }
+                </>
+              ) : (
+                <FaQuestionCircle className="text-4xl text-gray-400" />
+              )
+            }
           </div>
           <div>
             <div className="font-semibold">{`${transfer.recipient.firstName} ${transfer.recipient.lastName} (${transfer.recipient.phoneNumber})`}</div>
